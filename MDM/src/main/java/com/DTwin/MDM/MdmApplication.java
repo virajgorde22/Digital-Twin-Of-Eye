4932745329c1e8043307c07784a5f155f15a3c2c
virajgorde22 <virajgorde11@gmail.com>
2026-09-19 18:26:30 +0530

    Creation of Folder

---
+++ MDM/src/main/java/com/DTwin/MDM/MdmApplication.java
@@ -0,0 +1,13 @@
+package com.DTwin.MDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmApplication.class, args);
+	}
+
+}
